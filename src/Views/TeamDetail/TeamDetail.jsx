decb604a8c48f6efb3bc04e3fe001102420c4c6a
ProsperieEli <ElijahProsperie@gmail.com>
2021-12-09 19:32:54 -0500

    Finished with create, delete and update team

--- src/Views/TeamDetail/TeamDetail.jsx
+++ src/Views/TeamDetail/TeamDetail.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getTeamById } from "../../Services/teams";
+import { deleteTeam } from "../../Services/teams";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function TeamDetail({ label, match }) {
   const { teamId } = match.params;
   const [team, setTeam] = useState(null);
   const [loading, setLoading] = useState(true);
+  const history = useHistory();
 
   useEffect(() => {
     getTeamById(teamId)
@@ -15,6 +18,12 @@ export default function TeamDetail({ label, match }) {
   }, [teamId]);
 
   if (loading) return <h1>One Moment. . .</h1>;
+
+  const handleSubmit = async () => {
+    console.log("hi");
+    await deleteTeam(teamId);
+    history.push(`/teams`);
+  };
   return (
     <>
       <h2>{label}</h2>
@@ -22,7 +31,7 @@ export default function TeamDetail({ label, match }) {
         <Link to="/teams">Back to Teams</Link>
       </p>
       <p>
-        Coming from {team.city}, {team.state}
+        Coming from {team.city}, {team.state}.
       </p>
       <ul>
         {team.players.map((player) => {
@@ -35,7 +44,13 @@ export default function TeamDetail({ label, match }) {
           );
         })}
       </ul>
-      <button type="onclick">Delete Team</button>
+      <button type="submit" onClick={handleSubmit}>
+        Delete Team
+      </button>
+      <Link to={`/team/update/${team.id}`}>
+        {" "}
+        <button type="onClick">Update Team</button>
+      </Link>
     </>
   );
 }
